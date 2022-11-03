6b2a6d1c35c8d24567998fba00d49281e6f674f5
rockmonolit <rockmonolit@mail.ru>
2022-11-03 22:38:24 +0300

    feat: add solution to the fourth challenge

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+const sourceFolder = path.join(__dirname, "files");
+const copiedFolder = path.join(__dirname, "files-copy");
 
+fs.rm(copiedFolder, { force: true, recursive: true }, (err) => {
+  if (err) throw err;
+  fs.mkdir(copiedFolder, { recursive: true }, throwError);
+  fs.readdir(sourceFolder, (err, files) => {
+    if (err) throw err;
+    files.forEach((file) => {
+      const sourceFilePath = path.join(__dirname, "files", `${file}`);
+      const copiedFilePath = path.join(__dirname, "files-copy", `${file}`);
+      fs.copyFile(sourceFilePath, copiedFilePath, throwError);
+      
+    });
+  });
+});
+
+function throwError(err) {
+  if (err) throw err;
+}
